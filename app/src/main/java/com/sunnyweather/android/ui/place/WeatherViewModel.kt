a53203f4e886307c815c19ade15a5ee38fa148f6
wdf <a3561259700@126.com>
2025-07-07 23:06:31 +0800

    实现全球位置搜索，以及天气搜索的后端

---
+++ app/src/main/java/com/sunnyweather/android/ui/place/WeatherViewModel.kt
@@ -0,0 +1,25 @@
+package com.sunnyweather.android.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.model.Location
+
+class WeatherViewModel : ViewModel() {
+
+    var locationLng = ""
+    var locationLat = ""
+    var locationName = ""
+
+    private val locationLiveData = MutableLiveData<Location>()
+
+    val weatherLiveData = locationLiveData.switchMap { location ->
+        Repository.refreshWeather(location.lng, location.lat)
+    }
+
+    fun refreshWeather(lng: String, lat: String) {
+        locationLiveData.value = Location(lng, lat)
+    }
+
+}
